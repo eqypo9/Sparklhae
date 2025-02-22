02dc915dcac9a76dc9a69f07c5024937641f78fe
eqypo9 <eqypo@naver.com>
2025-02-22 23:22:22 +0900

    feat: 파비콘 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='fixed bottom-0 left-0 w-full bg-gradient-to-b from-gray-200 to-gray-100 text-black py-6 flex items-center justify-center text-center z-10'>
+    <footer className='bottom-0 left-0 w-full bg-gradient-to-b from-gray-200 to-gray-100 text-black py-6 flex items-center justify-center text-center z-10'>
       <p>&copy; {new Date().getFullYear()} Sparkle. All rights reserved.</p>
     </footer>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' defaultTheme='system'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+        <title>Sparkle | Portfolio</title>
+      </Head>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
