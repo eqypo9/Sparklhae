2fd0211d01d205a423f48e7d48e61952082ff106
eqypo9 <eqypo@naver.com>
2025-03-13 12:38:05 +0900

    feat: aboutData 분리

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -4,38 +4,7 @@ import GridBackground from '@/components/GridBackground';
 import Image from 'next/image';
 import astronautIcon from '../../../public/favicon.ico';
 import { ArrowLeft, ArrowRight, Award } from 'lucide-react';
-
-const messages = [
-  {
-    title: '사용자의 경험을 고민하는 개발자',
-    content:
-      '작은 디테일이 사용자 경험을 결정한다고 믿습니다. 미세한 UI 변화가 사용자에게 더 나은 경험을 제공할 수 있도록 고민합니다.',
-  },
-  {
-    title: '문제 해결을 통한 성장',
-    content:
-      'Global Nomad 프로젝트에서 반응형 UI 최적화를 해결하며 문제 해결 능력을 키웠고, 사용자 중심의 개발이 중요함을 다시금 깨달았습니다.',
-  },
-  {
-    title: '끈기와 소통의 힘',
-    content: '코드 리뷰를 통해 팀원들과 소통하며 더 나은 해결책을 찾아갑니다.',
-  },
-  {
-    title: '실질적인 가치를 전달하는 개발자',
-    content:
-      '웹 기술을 통해 실질적인 가치를 전달하고, 사용자들에게 편리함을 제공하는 것이 제가 개발을 하는 가장 큰 이유입니다.',
-  },
-];
-
-const certifications = [
-  {
-    name: '정보처리기사',
-    description: 'IT 전문 자격증',
-    date: '2023.05 필기 합격',
-  },
-  { name: 'SQLD', description: 'SQL 개발자 자격증', date: '2024.05 취득' },
-  { name: 'TOEIC', description: '영어 능력 시험', date: '2023.11(890점)' },
-];
+import { messages, certifications } from '@/data/aboutData';
 
 export default function AboutMe() {
   const [index, setIndex] = useState(0);
@@ -124,7 +93,7 @@ export default function AboutMe() {
 
         {/* 자격증 섹션 (경험 카드 스타일) */}
         <div className='flex flex-col items-center gap-6 mt-10'>
-          <h3 className='text-2xl font-bold text-white'>📜 보유 자격증</h3>
+          <h3 className='text-2xl font-bold text-white'>보유 자격증</h3>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
             {certifications.map((cert, i) => (
               <motion.div
