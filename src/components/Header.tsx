2f8488e31b5dfc9f055cba5eab7dd6648a68a03b
eqypo9 <eqypo@naver.com>
2025-03-14 16:38:33 +0900

    style: Header 반응형 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
     section: string
   ) => {
     e.preventDefault();
-    const path = '/'; 
+    const path = '/';
 
     if (window.location.pathname !== path) {
       router.push(`${path}#${section}`);
@@ -26,17 +26,18 @@ export default function Navbar() {
   };
 
   return (
-    <nav className='fixed top-0 w-full bg-darkBlue py-4 px-6 flex justify-between items-center shadow-lg z-50'>
-      <Link href='/' className='text-xl font-bold text-cosmic_teal'>
+    <nav className='fixed top-0 w-full bg-darkBlue py-3 px-4 md:px-6 flex items-center shadow-lg z-50'>
+      <Link href='/' className='text-lg md:text-xl font-bold text-cosmic_teal'>
         SparkleHae
       </Link>
-      <div className='flex gap-6'>
+
+      <div className='ml-auto flex gap-3 md:gap-6 text-xs md:text-base overflow-x-auto whitespace-nowrap'>
         {['About', 'Skills', 'Experience', 'Projects'].map((section) => (
           <a
             key={section}
             href={`#${section.toLowerCase()}`}
             className='text-white hover:text-cosmic_teal transition-colors duration-200 cursor-pointer'
-            onClick={(e) => handleNavigation(e, section.toLowerCase())} 
+            onClick={(e) => handleNavigation(e, section.toLowerCase())}
           >
             {section}
           </a>
